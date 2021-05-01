14542deb3f9dfb17359adca483e94de188cca701
Camila <camiilacguimaraes@gmail.com>
2021-05-01 15:29:59 -0300

    REACT NATIVE: aula 01
    * Tela de boas vindas
    * Imagens

--- plantManager/src/pages/Welcome.tsx
+++ plantManager/src/pages/Welcome.tsx
@@ -1,35 +1,56 @@
 import React from 'react';
-import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, Image } from 'react-native';
 
-import wateringImg from '../assets/watering.png'
+import { Button } from '../components/Button';
+import wateringImg from '../assets/watering.png';
+import colors from '../styles/colors';
+
+
 
 export function Welcome() {
   return (
-    <View>
-      <Text>
-        Gerencie suas plantas de forma fácil
+    <SafeAreaView style={ styles.container }>
+      <Text style={ styles.title }>
+        Gerencie {'\n'} suas plantas {'\n'} de forma fácil
       </Text>
 
-      <Image source={ wateringImg } />
+      <Image source={ wateringImg } style={ styles.image } />
 
-      <Text>
+      <Text style={ styles.subtitle }>
         Não esqueça mais de regar suas plantas. Nós cuidamos 
         de lembrar você sempre que precisar.
       </Text>
 
-      <TouchableOpacity>
-        <Text>
-          >
-        </Text>
-      </TouchableOpacity>
-    </View>
+      <Button title='>'/>      
+    </SafeAreaView>
   )
 }
 
-const style = StyleSheet.create({
+
+const styles = StyleSheet.create({
   container: {
     flex: 1, 
-    justifyContent: 'center', 
+    justifyContent: 'space-between', 
     alignItems: 'center'
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    //color: colors.heading,
+    marginTop: 38
+  },
+
+  image: {
+    width: 292,
+    height: 284
+  },
+   
+  subtitle: {
+    textAlign: 'center',
+    fontSize: 18,
+    paddingHorizontal: 20,
+    //color: colors.heading
   }
 })
